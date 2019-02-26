e874636aaeb2ec83ddc9f076d6d92e771df31a45
Jason Chin <jason.rob.chin@gmail.com>
2019-02-26 11:57:24 -0500

    add site section

---
+++ src/components/site-section.component.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class SiteSection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() { 
+    let { title } = this.props;
+    return (
+      <div className="site-section">
+        <section className="section">
+          <div className="header">
+            <h1 className="title is-2">{title}</h1>
+          </div>
+          <div className="content">
+            {this.props.children}
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+ 
+export default SiteSection;
