9ffdb9f0fbb29b7ac53d4c07e84ac36ad0085e03
Jason Chin <jason.rob.chin@gmail.com>
2019-02-26 11:57:41 -0500

    add experience cards

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './sass/app.scss';
 // Components
 import NavBar from './components/nav-bar.component';
 import Hero from './components/hero.component';
+import SiteSection from './components/site-section.component';
+import ExperienceCard from './components/experience-card.component';
+
+// Data
+import experience from './data/experience';
 
 class App extends Component {
   constructor(props) {
@@ -15,12 +20,42 @@ class App extends Component {
     };
   }
 
+  renderExperience = () => {
+    let cards = [];
+    
+    for (let i = 0; i < experience.length; i++) {
+      const e = experience[i];
+      cards.push(
+        <ExperienceCard
+          img={e.img}
+          position={e.position}
+          company={e.company}
+          location={e.location}
+          dates={e.dates}
+          length={e.length}
+        />
+      );
+    }
+
+    return cards;
+  }
+
   render() {
     return (
       <div id="site-container">
+      
         <NavBar />
 
         <Hero />
+
+        <SiteSection title="experience">
+          {this.renderExperience()}
+        </SiteSection>
+
+        <SiteSection title="projects">
+          site section
+        </SiteSection>
+
       </div>
     );
   }
---
+++ src/components/experience-card.component.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ExperienceCard extends Component {
+  render() {
+
+    let { img, company, position, location, dates, length } = this.props;
+
+    return (
+      <div className="card experience-card">
+        <article className="media">
+          <figure className="media-left">
+            <p className="image">
+              <img src={img} />
+            </p>
+          </figure>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <h1 className="title">{position}</h1>
+                {company} - {location}
+                <br/>
+                <small>{dates} <i className="fas fa-caret-right"></i> {length}</small>
+                <br/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+              </p>
+            </div>
+          </div>
+          <div className="media-right">
+            <button className="button is-white">
+              <i className="fas fa-angle-down"></i>
+            </button>
+          </div>
+        </article>
+      </div>
+    );
+  }
+}
+
+ExperienceCard.propTypes = {
+  img: PropTypes.string,
+  company: PropTypes.string,
+  position: PropTypes.string,
+  location: PropTypes.string,
+  dates: PropTypes.string,
+  length: PropTypes.string,
+  description: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.string
+  ])
+}
+ 
+export default ExperienceCard;
