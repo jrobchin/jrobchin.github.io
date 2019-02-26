66a8a701e5ed4e89de144fd5ce6ccecc57e14098
Jason Chin <jason.rob.chin@gmail.com>
2019-02-26 11:56:41 -0500

    add experience data and logos

---
+++ src/data/experience.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    img: "/img/DLR-logo.png",
+    position: "Undergraduate Researcher",
+    company: "German Aerospace Center (DLR)",
+    location: "Berlin, Germany",
+    dates: "May 2018 - Aug 2018",
+    length: "4 months",
+  },
+  {
+    img: "/img/Condos-logo.png",
+    position: "Full Stack Developer Intern",
+    company: "condos.ca",
+    location: "Toronto, Canada",
+    dates: "May 2017 - Sept 2017",
+    length: "5 months",
+  }
+];
