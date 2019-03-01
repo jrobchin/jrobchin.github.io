df53bee0e6f7a9601411009d26665b44152db251
Jason Chin <jason.rob.chin@gmail.com>
2019-02-28 19:31:01 -0500

    add animations to cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import React, { Component } from 'react';
 import './sass/app.scss';
 
+import { Waypoint } from 'react-waypoint';
+
 // Components
 import NavBar from './components/nav-bar.component';
 import Hero from './components/hero.component';
 import SiteSection from './components/site-section.component';
 import ExperienceCard from './components/experience-card.component';
+import ProjectCard from './components/project-card.component';
 
 // Data
 import experience from './data/experience';
+import projects from './data/projects';
+import posed from 'react-pose';
+
+const SlideInContainer = posed.div({
+  out: { x: "-100%" },
+  in: {
+    x: "0%",
+    delay: 500,
+    delayChildren: 500,
+    staggerChildren: 100,
+  }
+});
+
+const SlideInItem = posed.div({
+  out: { x: '-300%', opactity: 0 },
+  in: {
+    x: '0%',
+    opacity: 1
+  },
+});
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      populated: {
+        experience: false,
+        projects: false
+      }
+    };
+
     this.navRef = React.createRef();
     this.sectionRefs = {
       hero: React.createRef(),
       experience: React.createRef(),
       projects: React.createRef(),
     };
+    window.scrollTo(0, 0);
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      window.scrollTo(0, 0)
+    }, 100);
+  }
+
+  populateSection = (section) => {
+    this.setState({
+      populated: {
+        ...this.state.populated,
+        [section]: true
+      }
+    });
   }
 
   scrollTo = (section) => {
@@ -33,19 +79,45 @@ class App extends Component {
 
   renderExperience = () => {
     let cards = [];
-    
     for (let i = 0; i < experience.length; i++) {
       const e = experience[i];
       cards.push(
-        <ExperienceCard
+        <SlideInItem
+        key={i}
+        pose={this.state.populated.experience ? 'in' : 'out'}
+        >
+          <ExperienceCard
+            key={i}
+            img={e.img}
+            position={e.position}
+            company={e.company}
+            location={e.location}
+            dates={e.dates}
+            length={e.length}
+          />
+        </SlideInItem>
+      );
+    }
+
+    return cards;
+  }
+
+  renderProjects = () => {
+    let cards = [];
+    for (let i = 0; i < projects.length; i++) {
+      const p = projects[i];
+      cards.push(
+        <SlideInItem
           key={i}
-          img={e.img}
-          position={e.position}
-          company={e.company}
-          location={e.location}
-          dates={e.dates}
-          length={e.length}
-        />
+          className="column is-one-third" 
+          pose={this.state.populated.projects ? 'in' : 'out'}
+        >
+          <ProjectCard
+            img={p.img}
+            title={p.title}
+            description={p.description}
+          />
+        </SlideInItem>
       );
     }
 
@@ -55,22 +127,34 @@ class App extends Component {
   render() {
     return (
       <div id="site-container">
-      
+
         <NavBar domRef={this.navRef} scrollTo={this.scrollTo}/>
 
         <main style={{marginTop: 52}}>
 
           <Hero />
 
-          <SiteSection title="experience" sectionRef={this.sectionRefs.experience}>
-            {this.renderExperience()}
-          </SiteSection>
+          <Waypoint 
+            onEnter={() => this.populateSection('experience')}
+          />
 
-          <SiteSection title="projects" sectionRef={this.sectionRefs.projects}>
-            site section
-            {this.renderExperience()}
-          </SiteSection>
-          
+          <div style={{maxWidth: "1200px", margin: "auto"}}>
+            <SiteSection title="experience" sectionRef={this.sectionRefs.experience}>
+              <SlideInContainer pose={this.state.populated.experience ? 'in' : 'out'}>
+                {this.renderExperience()}
+              </SlideInContainer>
+            </SiteSection>
+
+            <Waypoint 
+              onEnter={() => this.populateSection('projects')}
+              />
+
+            <SiteSection title="projects" sectionRef={this.sectionRefs.projects}>
+              <SlideInContainer className="columns is-multiline" pose={this.state.populated.projects ? 'in' : 'out'}>
+                {this.renderProjects()}
+              </SlideInContainer>
+            </SiteSection>
+          </div>
         </main>
 
       </div>
