d824ea9ef8b3ae0eac0facc3835f91201a2113e7
Jason Chin <jason.rob.chin@gmail.com>
2019-02-28 19:31:08 -0500

    add projects data

---
+++ src/data/projects.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    img: "/img/airoyale-diagram.jpg",
+    title: "AI Royale",
+    description: "Redefining competitive coding and education with real-time bot battle competitions. (Winner at Hack Western 5)"
+  },
+  {
+    img: "/img/airoyale-diagram.jpg",
+    title: "AI Royale",
+    description: "Redefining competitive coding and education with real-time bot battle competitions. (Winner at Hack Western 5)"
+  },
+  {
+    img: "/img/airoyale-diagram.jpg",
+    title: "AI Royale",
+    description: "Redefining competitive coding and education with real-time bot battle competitions. (Winner at Hack Western 5)"
+  },
+  {
+    img: "/img/airoyale-diagram.jpg",
+    title: "AI Royale",
+    description: "Redefining competitive coding and education with real-time bot battle competitions. (Winner at Hack Western 5)"
+  },
+  {
+    img: "/img/airoyale-diagram.jpg",
+    title: "AI Royale",
+    description: "Redefining competitive coding and education with real-time bot battle competitions. (Winner at Hack Western 5)"
+  },
+  {
+    img: "/img/airoyale-diagram.jpg",
+    title: "AI Royale",
+    description: "Redefining competitive coding and education with real-time bot battle competitions. (Winner at Hack Western 5)"
+  },
+  {
+    img: "/img/airoyale-diagram.jpg",
+    title: "AI Royale",
+    description: "Redefining competitive coding and education with real-time bot battle competitions. (Winner at Hack Western 5)"
+  },
+  {
+    img: "/img/airoyale-diagram.jpg",
+    title: "AI Royale",
+    description: "Redefining competitive coding and education with real-time bot battle competitions. (Winner at Hack Western 5)"
+  },
+  {
+    img: "/img/airoyale-diagram.jpg",
+    title: "AI Royale",
+    description: "Redefining competitive coding and education with real-time bot battle competitions. (Winner at Hack Western 5)"
+  }
+];
